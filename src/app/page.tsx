25b159aaf3f404447f9c4b6d3eb482475081084f
Marco Meijer <marcomeijerm@gmail.com>
2023-03-24 11:32:54 +0100

    feat: added graphql getMovieLists query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { EMAIL } from "@/constants/email";
+import { sdk } from "@/lib/client";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <main className={styles.main}></main>;
+export default async function Home() {
+  const res = await sdk.Query({
+    email: EMAIL,
+  });
+
+  return (
+    <main className={styles.main}>
+      {res.getMovieLists.map((list) => (
+        <p key={list.id}>{list.name}</p>
+      ))}
+    </main>
+  );
 }
