ef6fabab42c2fb051f7df7cfa4063ba53085748b
Marco Meijer <marcomeijerm@gmail.com>
2023-03-25 18:19:47 +0100

    feat: improve movie page

--- src/app/movie/[imdbId]/page.tsx
+++ src/app/movie/[imdbId]/page.tsx
@@ -1,5 +1,7 @@
+import StarIcon from "@/components/icons/StarIcon";
 import { sdk } from "@/lib/client";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export interface MoviePageProps {
   params: { imdbId: string };
@@ -15,10 +17,48 @@ export default async function MoviePage({
   }
 
   return (
-    <div>
-      <Image width={400} height={600} src={movie.Poster!!} alt="movie poster" />
-      <h1>{movie.Title}</h1>
-      <p>{movie.Plot}</p>
+    <div className={styles.movieInfo}>
+      <h1 className={styles.title}>{movie.Title}</h1>
+      <div className={styles.underTitle}>
+        <p>{movie.Year}</p>
+        <p>{movie.Rated}</p>
+        <p>{movie.Runtime}</p>
+        <div className={styles.rating}>
+          <StarIcon
+            fill="#fbd341"
+            width={26}
+            height={26}
+            style={{ marginBottom: 4, marginRight: 4 }}
+          />
+          {movie.imdbRating}
+        </div>
+      </div>
+      <div className={styles.content}>
+        <Image src={movie.Poster || ""} width={260} height={386} alt={""} />
+        <div className={styles.description}>
+          <p>{movie.Plot}</p>
+          <div className={styles.section}>
+            <h1>Director</h1>
+            <p>{movie.Director}</p>
+          </div>
+          <div className={styles.section}>
+            <h1>Actors</h1>
+            {movie.Actors?.split(", ").map((actor, i) => (
+              <p key={i}>{actor}</p>
+            ))}
+          </div>
+          <div className={styles.section}>
+            <h1>Genre</h1>
+            {movie.Genre?.split(", ").map((genre, i) => (
+              <p key={i}>{genre}</p>
+            ))}
+          </div>
+          <div className={styles.section}>
+            <h1>Awards</h1>
+            {movie.Awards}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
