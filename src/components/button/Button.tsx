0b4fd9f69f5b1c69518cdee4155e69e921795674
Marco Meijer <marcomeijerm@gmail.com>
2023-03-25 19:25:00 +0100

    style: button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,14 @@
+import styles from "./Button.module.css";
+
 export interface ButtonProps {
   title: string;
   onClick?: () => void;
 }
 
 export function Button({ title, onClick }: ButtonProps) {
-  return <button onClick={onClick}>{title}</button>;
+  return (
+    <button onClick={onClick} className={styles.button}>
+      {title}
+    </button>
+  );
 }
