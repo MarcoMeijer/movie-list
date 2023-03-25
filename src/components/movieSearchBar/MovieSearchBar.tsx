bb63805072cc50c45e69797f3578f6b29d3586b5
Marco Meijer <marcomeijerm@gmail.com>
2023-03-25 12:21:09 +0100

    style: dark mode

--- src/components/movieSearchBar/MovieSearchBar.tsx
+++ src/components/movieSearchBar/MovieSearchBar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styles from "./MovieSearchBar.module.css"
 import { sdk } from "@/lib/client";
 import { SearchMovie } from "@/generated/graphql";
+import Image from "next/image";
 
 function filterNull<T>(x: null | T): x is T {
   return x !== null;
@@ -58,8 +59,14 @@ export default function MovieSearchBar({ onAdd }: MovieSearchBarProps): JSX.Elem
     {
       searchResult.map((movie) => (
         <div className={styles.searchItem} onClick={() => onAdd(movie)}>
-          <h1>{movie.Title}</h1>
-          <p>{movie.Type} - {movie.Year}</p>
+          {
+            movie.Poster && movie.Poster !== "N/A" &&
+            <Image src={movie.Poster} width={32} height={48} alt={`poster ${movie.Title}`} style={{ marginRight: 10 }} />
+          }
+          <div className={styles.searchItemText}>
+            <h1>{movie.Title}</h1>
+            <p>{movie.Type} - {movie.Year}</p>
+          </div>
         </div>
       ))
     }
